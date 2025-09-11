488ad71ef8d669924431b0059886dbfd7aff8ad2
GoulartG16 <geovanagoulart776@gmail.com>
2025-09-11 18:23:52 -0300

    Update ex2.c

--- Slide 07 - Structs/ex2.c
+++ Slide 07 - Structs/ex2.c
@@ -1,12 +1,24 @@
-
-
-struct Personagem preencheA (void) {
+#include <stdio.h>
+#include <stdlib.h>
+struct Personagem {
+    int forca;
+    int energia;
+    int experiencia;
+};
+struct Personagem preencheA (struct Personagem p) {
+    printf("Força: ");
+    scanf("%d", &p.forca);
+    printf("Energia: ");
+    scanf("%d", &p.energia);
+    printf("Experiência: ");
+    scanf("%d", &p.experiencia);
+    return p;
 }
-
-void main (void) {
-    struct Personagem p1 = preencheA();
-    struct Personagem p2 = preencheA();
+int main () {
+    struct Personagem p1 = preencheA(p1);
+    struct Personagem p2 = preencheA(p2);
     printf("(%d,%d,%d) vs (%d,%d,%d)\n",
            p1.forca, p1.energia, p1.experiencia,
            p2.forca, p2.energia, p2.experiencia);
+    return 0;
 }
