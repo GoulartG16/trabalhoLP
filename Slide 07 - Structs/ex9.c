2d92775cb9b7dee9844c8839c54deedf08cd77f7
GoulartG16 <geovanagoulart776@gmail.com>
2025-09-12 15:58:49 -0300

    Create ex9.c

---
+++ Slide 07 - Structs/ex9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+typedef struct {
+    int x;
+    int y;
+} coord;
+typedef struct {
+    int identidade;
+    coord posicao;
+    int pontuacao;
+} personagem;
+int main(){
+    personagem personagens[10];
+    return 0;
+}
