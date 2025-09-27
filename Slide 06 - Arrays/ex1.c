2dee32eda8f8c7f33c4ba34ecca92e14b02f9241
GoulartG16 <geovanagoulart776@gmail.com>
2025-09-27 20:26:24 -0300

    Create ex1.c

---
+++ Slide 06 - Arrays/ex1.c
@@ -0,0 +1,8 @@
+void preenche(int arr[L][C], int l, int c) {
+    for (int i = 0; i < l; i++){
+        for(int j = 0; j < c) {
+            printf("Elemento %dx%d", i+1, j+1);
+            scanf(%d, &arr[i][j]);
+        }
+    }
+}
