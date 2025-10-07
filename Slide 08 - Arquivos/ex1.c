188358d6346b8ab472253a3dc989f025a89404ca
GoulartG16 <geovanagoulart776@gmail.com>
2025-10-07 14:12:44 -0300

    Create ex1.c

---
+++ Slide 08 - Arquivos/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[25];
+    printf("String: ");
+    fgets(str, 25, stdin);
+    FILE* f = fopen("arquivo.txt", "w");
+    int i = 0;
+    while (str[i]!= '\0'){
+        fputc(str[i], f);
+        i++;
+    }
+    return 0;
+}
