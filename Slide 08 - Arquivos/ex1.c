2764dab87edc6fb2a96ce4d47409b44b81ad25c0
GoulartG16 <geovanagoulart776@gmail.com>
2025-10-11 11:47:49 -0300

    a

--- Slide 08 - Arquivos/ex1.c
+++ Slide 08 - Arquivos/ex1.c
@@ -5,7 +5,7 @@ int main(){
     char str[25];
     printf("String: ");
     fgets(str, 25, stdin);
-    FILE* f = fopen("arquivo.txt", "w");
+    FILE* f = fopen("C:\\arquivosc\\arquivo.txt", "w");
     int i = 0;
     while (str[i]!= '\0'){
         fputc(str[i], f);
--- Slide 08 - Arquivos/ex2.c
+++ Slide 08 - Arquivos/ex2.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
-  char str[25], aux;
-  int i=0;
-  FILE* f = fopen("arquivo.txt", "r");
+  char str[25];
+  int i=0, aux;
+  FILE* f = fopen("C:\\arquivosc\\arquivo.txt", "r");
   aux = fgetc(f);
   while (aux != EOF){
-    strcpy(str[i], aux);
+    str[i] = (char)aux;
     i++;
     aux = fgetc(f);
   }
+  str[i] = '\0';
   printf("%s", str);
   return 0;
 }
